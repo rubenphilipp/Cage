247cb69593df355c76fe06e05ce4dfa1d3000e7f
Ruben Philipp <rp@rubylon.de>
2017-06-04 22:12:40 +0200

    implemented DC motor

--- Cage1.ino
+++ Cage1.ino
@@ -17,13 +17,20 @@
 
 // initialize pins
 const int SERVO = 3; // servo motor for the mallet (PWM required) {ARD: 3; BB: a-e28}
+
 const int dataPin = 2; // DS [S1] on 74HC595 {ARD: 2; BB: d13}
 const int latchPin = 12; // ST_CP [RCK] on 74HC595 {ARD: 12; BB: d11}
 const int clockPin = 13; // SH_CP [SCK] on 74HC595 {ARD: 13; BB: d10}
+
 const int stepper1 = 8; // {ARD: 8; STP: 1}
 const int stepper2 = 9; // {ARD: 9; STP: 2}
 const int stepper3 = 10; // {ARD: 10; STP: 3}
 const int stepper4 = 11; // {ARD: 11; STP: 4}
+
+/* DC motor driver (L293D) */
+const int DCENABLE = 5; // {ARD: 5; BB: j51}
+const int DCDIRA = 4; // {ARD: 4; BB: j52}
+const int DCDIRB = 6; // {ARD: 6; BB: j46}
 
 
 // initialize Servo object
@@ -48,10 +55,15 @@ void setup() {
 
   /* set up pins: */
 
-  // shift register
+  /* shift register */
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);  
   pinMode(clockPin, OUTPUT);
+
+  /* DC motor */
+  pinMode(DCENABLE,OUTPUT);
+  pinMode(DCDIRA,OUTPUT);
+  pinMode(DCDIRB,OUTPUT);
 
   updateShiftRegister(); //initial shift register update
 
@@ -76,7 +88,14 @@ void loop() {
   
   /*stepper.setSpeed(1200);
   stepper.step(stepsPerOutputRevolution/8);*/
+  
+  digitalWrite(DCENABLE, HIGH); // enable on
+  digitalWrite(DCDIRA, HIGH);
+  digitalWrite(DCDIRB, LOW);
   delay(500);
+  
+  digitalWrite(DCENABLE, LOW); // enable off
+  delay(2000);
 
 }
 
